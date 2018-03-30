0bf8669e3361bd29c9ec2ea14c7a70d993f5a298
Sergey Striga <sergey@Sergeys-MacBook-Pro.local>
2018-03-30 12:45:42 +0300

    add encodedcommand

--- bin/winrm-ps-exec.py
+++ bin/winrm-ps-exec.py
@@ -4,7 +4,19 @@ import json
 import winrm
 import sys
 
-session = winrm.Session(sys.argv[1], auth=(sys.argv[2], sys.argv[3]))
-result = session.run_ps(sys.argv[4])
 
-print(json.dumps({'std_out': result.std_out, 'std_err': result.std_err, 'status_code': result.status_code}))
+def fix_run_ps(self, script):
+    from base64 import b64encode
+    encoded_ps = b64encode(script.encode('utf_16_le')).decode('ascii')
+    rs = self.run_cmd('powershell -encodedcommand {0}'.format(encoded_ps))
+    if len(rs.std_err):
+        rs.std_err = self._clean_error_msg(rs.std_err.decode('utf-8'))
+    return rs
+
+try:
+    winrm.Session.run_ps = fix_run_ps
+    session = winrm.Session(sys.argv[1], auth=(sys.argv[2], sys.argv[3]))
+    result = session.run_ps(sys.argv[4])
+    print(json.dumps({'std_out': str(result.std_out, 'utf-8'), 'std_err': result.std_err, 'status_code': result.status_code}))
+except BaseException as e:
+    print(json.dumps({'std_out': '', 'std_err': str(e), 'status_code': 1}))
